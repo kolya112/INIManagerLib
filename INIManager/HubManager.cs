472abb0efbe00893b56713d0ebe43b1723f5202c
kolya112 <nikolayurch@gmail.com>
2021-12-21 22:26:19 +0300

    Добавьте файлы проекта.

---
+++ INIManager/HubManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INIManager
+{
+    public class HubManager
+    {
+        //Конструктор, принимающий путь к INI-файлу
+        public HubManager(string aPath)
+        {
+            path = aPath;
+        }
+
+        //Конструктор без аргументов (путь к INI-файлу нужно будет задать отдельно)
+        public HubManager() : this("") { }
+
+        //Возвращает значение из INI-файла (по указанным секции и ключу) 
+        public string GetString(string aSection, string aKey)
+        {
+            //Для получения значения
+            StringBuilder buffer = new StringBuilder(SIZE);
+
+            //Получить значение в buffer
+            GetString(aSection, aKey, null, buffer, SIZE, path);
+
+            //Вернуть полученное значение
+            return buffer.ToString();
+        }
+
+        //Пишет значение в INI-файл (по указанным секции и ключу) 
+        public void WriteString(string aSection, string aKey, string aValue)
+        {
+            //Записать значение в INI-файл
+            WriteString(aSection, aKey, aValue, path);
+        }
+
+        //Возвращает или устанавливает путь к INI файлу
+        public string Path { get { return path; } set { path = value; } }
+
+        //Поля класса
+        private const int SIZE = 1024; //Максимальный размер (для чтения значения из файла)
+        private string path = null; //Для хранения пути к INI-файлу
+
+        //Импорт функции GetPrivateProfileString (для чтения значений) из библиотеки kernel32.dll
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileString")]
+        private static extern int GetString(string section, string key, string def, StringBuilder buffer, int size, string path);
+
+        //Импорт функции WritePrivateProfileString (для записи значений) из библиотеки kernel32.dll
+        [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileString")]
+        private static extern int WriteString(string section, string key, string str, string path);
+    }
+}
